127d6d58505301d2ac8280094a9ac11da3d3527f
Li Bo <borgerli@tencent.com>
2020-05-13 17:11:39 +0800

    support for lxcfs 4.0.3

--- lxcfs-image/start.sh
+++ lxcfs-image/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#docker run -it --pid host --privileged -v /usr/local:/usr/local -v /sys/fs/cgroup:/sys/fs/cgroup -v /var/lib/lxcfs:/var/lib/lxcfs  -v /usr/lib64:/usr/lib64 lxcfs:4.0.3
 # Cleanup
 nsenter -m/proc/1/ns/mnt fusermount -u /var/lib/lxcfs 2> /dev/null || true
 nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
@@ -11,6 +11,8 @@ mkdir -p /usr/local/lib/lxcfs /var/lib/lxcfs
 # Update lxcfs
 cp -f /lxcfs/lxcfs /usr/local/bin/lxcfs
 cp -f /lxcfs/liblxcfs.so /usr/local/lib/lxcfs/liblxcfs.so
+cp -f /lxcfs/libfuse.so.2 /lib64/libfuse.so.2
+cp -f /lxcfs/libulockmgr.so.1 /lib64/libulockmgr.so.1
 
 
 # Mount
