316f7b7f2a4b5dcaefc0ba6879786c08a5f665bf
Li Bo <borgerli@tencent.com>
2020-05-14 16:59:05 +0800

    fix cpuinfo problem

--- lxcfs-image/start.sh
+++ lxcfs-image/start.sh
@@ -2,4 +2,4 @@
 #docker run -it --privileged -v /var/lib/lxcfs:/var/lib/lxcfs:rshared --pid host lxcfs:4.0.3
 #docker run -it -m 128m -v /var/lib/lxcfs/proc/meminfo:/proc/meminfo centos:7 free -m
 
-exec /usr/local/bin/lxcfs /var/lib/lxcfs/
+exec /usr/local/bin/lxcfs --enable-cfs --enable-pidfd /var/lib/lxcfs/
